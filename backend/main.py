56d7052349a79446c0c528c4062b7f621b279963
lz-chen <lingzhen.chen@inmeta.no>
2024-09-03 22:47:59 +0200

    feat: implement streaming response for slide generation workflow

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,46 @@
 from fastapi import FastAPI, HTTPException
 import asyncio
+from fastapi.responses import StreamingResponse
 
-from backend.models import SlideGenFileDirectory
-from workflows.slide_gen_agent import run_workflow
+from models import SlideGenFileDirectory
+from workflows.slide_gen_agent import run_workflow, SlideGenWorkflow
 
 app = FastAPI()
 
 
 @app.post("/run-slide-gen")
 async def run_workflow_endpoint(file_dir: SlideGenFileDirectory):
-    try:
-        asyncio.run(run_workflow(file_dir.path))
-        return {"status": "Workflow completed successfully"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    wf = SlideGenWorkflow(timeout=1200, verbose=True)
+
+    def generate():
+        task = asyncio.create_task(wf.run(file_dir=file_dir.path))
+        async for ev in wf.stream_events():
+            yield ev.msg
+    return StreamingResponse(generate(), media_type="text/event-stream")
+
+    # def generate():
+    #     streaming_response = wf.run(
+    #         file_dir=file_dir.path,
+    #     )
+    #     for text in streaming_response.:
+    #         yield f"data: {text}\n\n"
+    #
+    # return StreamingResponse(generate(), media_type="text/event-stream")
+    # def generate():
+    #     response = await run_workflow(file_dir.path)
+    #     for text in response.response_gen:
+    #         yield f"data: {text}\n\n"
+    #
+    #
+    # try:
+    #     response = await run_workflow(file_dir.path)
+    #     async for token in response:
+    #         print(token.delta, end="", flush=True)
+    #     # return {"status": "Workflow completed successfully"}
+    #     return StreamingResponse(generate(), media_type="text/event-stream")
+    #
+    # except Exception as e:
+    #     raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.get("/items/{item_id}")
