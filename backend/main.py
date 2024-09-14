3186e311f4124aaca6549a1c39cd2eb9a6522ecd
lz-chen <lingzhen.chen@inmeta.no>
2024-09-14 21:49:04 +0200

    feat: update slide generation workflow to include summarization and improve event response format

--- backend/main.py
+++ backend/main.py
@@ -3,24 +3,34 @@ import asyncio
 from fastapi.responses import StreamingResponse
 import uuid
 import json
-from models import SlideGenFileDirectory
+from models import SlideGenFileDirectory, ResearchTopic
 from workflows.slide_gen import SlideGenerationWorkflow
+from workflows.summarize_and_generate_slides import SummaryAndSlideGenerationWorkflow
+from workflows.summary_gen import SummaryGenerationWorkflow
 
 app = FastAPI()
 workflows = {}  # Store the workflow instances in a dictionary
 
 
 @app.post("/run-slide-gen")
-async def run_workflow_endpoint(file_dir: SlideGenFileDirectory):
+async def run_workflow_endpoint(topic: ResearchTopic):
     workflow_id = str(uuid.uuid4())
-    wf = SlideGenerationWorkflow(timeout=1200, verbose=True)
+
+    wf = SummaryAndSlideGenerationWorkflow(timeout=2000, verbose=True)
+    wf.add_workflows(
+        summary_gen_wf=SummaryGenerationWorkflow(timeout=800, verbose=True)
+    )
+    wf.add_workflows(slide_gen_wf=SlideGenerationWorkflow(timeout=1200, verbose=True))
+
+    # wf = SlideGenerationWorkflow(timeout=1200, verbose=True)
+
     workflows[workflow_id] = wf  # Store the workflow instance
 
     async def event_generator():
         loop = asyncio.get_running_loop()
         print(f"event_generator: loop id {id(loop)}")
-        yield json.dumps({"workflow_id": workflow_id}) + "\n\n"
-        task = asyncio.create_task(wf.run(file_dir=file_dir.path))
+        yield f"{json.dumps({'workflow_id': workflow_id})}\n\n"
+        task = asyncio.create_task(wf.run(user_query=topic.query))
         print(f"event_generator: Created task {task}")
         try:
             async for ev in wf.stream_events():
@@ -28,11 +38,12 @@ async def run_workflow_endpoint(file_dir: SlideGenFileDirectory):
                 yield f"{ev.msg}\n\n"
                 await asyncio.sleep(0.1)  # Small sleep to ensure proper chunking
             final_result = await task
-            yield f"[Final result]: {final_result}\n\n"
+            # yield f"[Final result]: {final_result}\n\n"
+            yield f"{json.dumps({'final_result': final_result})}\n\n"
         except Exception as e:
             error_message = f"Error in workflow: {str(e)}"
             print(error_message)
-            yield f"data: {json.dumps({'event': 'error', 'message': error_message})}\n\n"
+            yield f"{json.dumps({'event': 'error', 'message': error_message})}\n\n"
         finally:
             # Clean up
             workflows.pop(workflow_id, None)
